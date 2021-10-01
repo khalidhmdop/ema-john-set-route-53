6ca937d487507c924ed59228a1d5c95f0616a497
khalidhmdop <khalidhasanct@gmail.com>
2021-10-01 12:47:37 +0600

    headline added

---
+++ src/components/Inentory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Hi, I am Khalid Hasan</h1>
+        </div>
+    );
+};
+
+export default Inventory;
